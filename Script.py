210def983ec7e697b7a6c94f45f13cf4390f47ca
amccreation <84769383+amccreation@users.noreply.github.com>
2022-03-17 20:56:35 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -4,22 +4,21 @@ I'm <a href=https://t.me/{}>{}</a>, ADD ME TO YOUR GROUP.... I WILL PROVIDE MOVI
     HELP_TXT = """𝙷𝙴𝚈 {}
 𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
     ABOUT_TXT = """ I'M {}
-SOURCE CODE CREDITS : <a> https://telegram.dog/EvaMariaDevs </a>
+SOURCE CODE CREDITS : <a> href=https://telegram.dog/EvaMariaDevs </a>
 Made with PYROGRAM
 HOSTED IN Raspberry pi4 🌡️🌡️🥲
  Maintained- @vishnuprasad01 """
-    SOURCE_TXT = """<b>NOTE:</b>
-- Eva Maria is a open source project. 
-- Source - <a> https://t.me/amc_status_bot</a>
+    SOURCE_TXT = """<b>NOTE:</b> 
+- Source - <a> href=https://t.me/amc_status_bot</a>
 
-<b>DEVS:</b>
-- <a href=https://t.me/amc_status_bot>Team Eva Maria</a>"""
+<b>ask movies:</b>
+- <a href=https://t.me/amc_status_bot>Ask movies </a>"""
     MANUELFILTER_TXT = """Help: <b>Filters</b>
 
 - Filter is the feature were users can set automated replies for a particular keyword and EvaMaria will respond whenever a keyword is found the message
 
 <b>NOTE:</b>
-1. eva maria should have admin privillage.
+1. I should have admin privillage.
 2. only admins can add filters in a chat.
 3. alert buttons have a limit of 64 characters.
 
@@ -30,11 +29,11 @@ HOSTED IN Raspberry pi4 🌡️🌡️🥲
 • /delall - <code>delete the whole filters in a chat (chat owner only)</code>"""
     BUTTON_TXT = """Help: <b>Buttons</b>
 
-- Eva Maria Supports both url and alert inline buttons.
+- Supports both url and alert inline buttons.
 
 <b>NOTE:</b>
 1. Telegram will not allows you to send buttons without any content, so content is mandatory.
-2. Eva Maria supports buttons with any telegram media type.
+2. supports buttons with any telegram media type.
 3. Buttons should be properly parsed as markdown format
 
 <b>URL buttons:</b>
@@ -65,7 +64,7 @@ HOSTED IN Raspberry pi4 🌡️🌡️🥲
     EXTRAMOD_TXT = """Help: <b>Extra Modules</b>
 
 <b>NOTE:</b>
-these are the extra features of Eva Maria
+these are the extra features
 
 <b>Commands and Usage:</b>
 • /id - <code>get id of a specified user.</code>
@@ -90,10 +89,10 @@ This module only works for my admins
 • /channel - <code>to get list of total connected channels</code>
 • /broadcast - <code>to broadcast a message to all users</code>"""
     STATUS_TXT = """★ 𝚃𝙾𝚃𝙰𝙻 𝙵𝙸𝙻𝙴𝚂: <code>{}</code>
-★ 𝚃𝙾𝚃𝙰𝙻 𝚄𝚂𝙴𝚁𝚂: <code>{}</code>
-★ 𝚃𝙾𝚃𝙰𝙻 𝙲𝙷𝙰𝚃𝚂: <code>{}</code>
-★ 𝚄𝚂𝙴𝙳 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱
-★ 𝙵𝚁𝙴𝙴 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱"""
+ 𝚃𝙾𝚃𝙰𝙻 𝚄𝚂𝙴𝚁𝚂: <code>{}</code>
+ 𝚃𝙾𝚃𝙰𝙻 𝙲𝙷𝙰𝚃𝚂: <code>{}</code>
+ 𝚄𝚂𝙴𝙳 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> gb
+ 𝙵𝚁𝙴𝙴 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> gb"""
     LOG_TEXT_G = """#NewGroup
 Group = {}(<code>{}</code>)
 Total Members = <code>{}</code>
